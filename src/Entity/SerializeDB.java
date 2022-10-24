2b7adad603abbf51da8a4a32a2b60bac7af1a4b8
AmosChong <amoschong0820@gmail.com>
2022-10-24 14:27:48 +0800

    Modify codes

--- src/Entity/SerializeDB.java
+++ src/Entity/SerializeDB.java
@@ -5,19 +5,20 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.List;
-import java.util.ArrayList;
+import java.io.EOFException;
 
 public class SerializeDB {
-    public static List readSerializedObject(String filename) {
-        List pDetails = null;
+    public static Object readSerializedObject(String filename) {
+        Object myObj = null;
         FileInputStream fis = null;
         ObjectInputStream in = null;
         try {
             fis = new FileInputStream(filename);
             in = new ObjectInputStream(fis);
-            pDetails = (ArrayList) in.readObject();
+            myObj = in.readObject();
             in.close();
+        } catch (EOFException ex) {
+            return null;
         } catch (IOException ex) {
             ex.printStackTrace();
         } catch (ClassNotFoundException ex) {
@@ -26,16 +27,16 @@ public class SerializeDB {
         // print out the size
         // System.out.println(" Details Size: " + pDetails.size());
         // System.out.println();
-        return pDetails;
+        return myObj;
     }
 
-    public static void writeSerializedObject(String filename, List list) {
+    public static void writeSerializedObject(String filename, Object myObj) {
         FileOutputStream fos = null;
         ObjectOutputStream out = null;
         try {
             fos = new FileOutputStream(filename);
             out = new ObjectOutputStream(fos);
-            out.writeObject(list);
+            out.writeObject(myObj);
             out.close();
             // System.out.println("Object Persisted");
         } catch (IOException ex) {
