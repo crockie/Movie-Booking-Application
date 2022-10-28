196b8796aa266037d43efc5fc0ce4e1e91fb44a6
AmosChong <amoschong0820@gmail.com>
2022-10-28 20:53:43 +0800

    Added Javadoc comment

--- src/Entity/CinemaStaff.java
+++ src/Entity/CinemaStaff.java
@@ -1,18 +1,47 @@
 package Entity;
 
+/**
+ * Serves as the login system of cinema staff
+ */
 public class CinemaStaff {
+
+    /**
+     * The username of the cinema staff
+     */
     private String username;
+
+    /**
+     * The password that the cinema staff uses to login the system
+     */
     private String password;
 
+    /**
+     * Create a CinemaStaff object with username and password provided
+     * 
+     * @param us  Username of the cinema staff
+     * @param pwd Password of the cinema staff
+     */
     public CinemaStaff(String us, String pwd) {
         username = us;
         password = pwd;
     }
 
+    /**
+     * For cinema staff to change password
+     * 
+     * @param pwd New password of the cinema staff
+     */
     public void setPassword(String pwd) {
         password = pwd;
     }
 
+    /**
+     * For cinema staff to login into the system
+     * 
+     * @param pwd Password inputted by the cinema staff
+     * @return If password is matched, return true to allow access
+     *         Else, return false to deny access
+     */
     public boolean login(String pwd) {
         if (password.equals(pwd))
             return true;
@@ -20,6 +49,11 @@ public class CinemaStaff {
             return false;
     }
 
+    /**
+     * Get the username of the cinema staff
+     * 
+     * @return the username of the cinema staff
+     */
     public String getUsername() {
         return username;
     }
