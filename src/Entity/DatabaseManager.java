5fd611cfa677a1f861b3688d1bbf576e2f1d7e4f
AmosChong <amoschong0820@gmail.com>
2022-10-28 20:19:13 +0800

    Rename DataController to DatabaseManager and Added Javadoc comments

---
+++ src/Entity/DatabaseManager.java
@@ -0,0 +1,61 @@
+package Entity;
+
+/**
+ * Manages data stored in database
+ */
+public class DataController {
+
+    /**
+     * To store data
+     */
+    private static DataBase database;
+
+    /**
+     * Relative path of the data file
+     */
+    private static String path = "";
+
+    /**
+     * Constructor of the class
+     */
+    public DataController() {
+    }
+
+    /**
+     * Initialize a new DataBase object
+     */
+    public static void init() {
+        database = new DataBase();
+    }
+
+    /**
+     * Read the file
+     * If the file is not existed or the file is not a DataBase object, call init()
+     * Else, downcast the file to DataBase type
+     */
+    public static void read() {
+        Object newObj = SerializeDB.readSerializedObject(path);
+
+        if (newObj == null || !(newObj instanceof DataBase))
+            init();
+        else
+            database = (DataBase) newObj;
+    }
+
+    /**
+     * Write to the file
+     */
+    public static void write() {
+        SerializeDB.writeSerializedObject(path, database);
+    }
+
+    /**
+     * Return the database
+     * 
+     * @return
+     */
+    public DataBase getDataBase() {
+        return database;
+    }
+
+}
