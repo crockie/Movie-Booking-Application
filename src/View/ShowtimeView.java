b8dc125140a86229c1496ddeb1f764509aac74a2
AmosChong <amoschong0820@gmail.com>
2022-10-29 13:06:07 +0800

    Added ShowtimeView

---
+++ src/View/ShowtimeView.java
@@ -0,0 +1,55 @@
+package View;
+
+import Entity.DatabaseManager;
+import Entity.Cineplex;
+import Entity.DataBase;
+import Entity.MovieTime;
+import Entity.Movie;
+import Entity.ShowStatus;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * This class shows all showtimes
+ */
+public class ShowtimeView {
+
+    /**
+     * This method displays all showtimes for all 'PREVIEW' and 'NOW SHOWING' movie
+     * in all cineplexes
+     */
+    public static void getShowtimeView() {
+        DataBase database = DatabaseManager.getDataBase();
+        // Access database to get cineplex
+        List<Cineplex> cineplexList = database.getCineplexList();
+
+        for (Cineplex cineplex : cineplexList) {
+            System.out.println("The name of the cineplex is: " + cineplex.getName());
+
+            // Get showtimes in each cineplex
+            List<MovieTime> movieTimeList = cineplex.getMovieTimes();
+            // Group showtime with the particular movie
+            Map<Movie, List<MovieTime>> movieTime = movieTimeList.stream()
+                    .collect(Collectors.groupingBy(MovieTime::getMovie));
+
+            for (Map.Entry<Movie, List<MovieTime>> movieAndTime : movieTime.entrySet()) {
+                Movie movie = movieAndTime.getKey();
+
+                ShowStatus movieShowStatus = movie.getShowStatus();
+                if (movieShowStatus == ShowStatus.NOW_SHOWING || movieShowStatus == ShowStatus.PREVIEW) {
+                    List<MovieTime> showTimeList = movieAndTime.getValue();
+                    System.out.println("The name of the movie is: " + movie.getTitle());
+
+                    // Sort the date and time when a showtime is added
+
+                    for (MovieTime showtime : showTimeList) {
+                        // Not done...Waiting for method to be created in MovieTime class
+                        System.out.println(showtime);
+                    }
+                }
+            }
+        }
+    }
+}
