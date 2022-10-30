b2944e14380b8d82e6c5811eeccb23a56b6a9acb
raydent30 <102127369+raydent30@users.noreply.github.com>
2022-10-30 09:52:29 +0800

    Update MovieRating.java

--- src/Entity/MovieRating.java
+++ src/Entity/MovieRating.java
@@ -4,52 +4,53 @@ package Entity;
  * The movie ratings 
  */
 public enum MovieRating implements ItemName {
-   	/**
-	 * General
-	 */
+
+   /**
+   * General
+   */
     G("G"),
 
     /**
-	 * Parental Guidance
-	 */
+    * Parental Guidance
+    */
     PG("PG"),
 
     /**
-	 * Parental Guidance 13
-	 */
+    * Parental Guidance 13
+    */
     PG13("PG13"),
 
     /**
-	 * No Children Under 16
-	 */
+    * No Children Under 16
+    */
     NC16("NC16"),
 
     /**
-	 * Mature 18
-	 */
+    * Mature 18
+    */
     M18("M18"),
 	
     /**
-	 * Restricted 21
-	 */
+    * Restricted 21
+    */
     R21("R21");
 
     /**
-	 * The name of the release rating
-	 */
+    * The name of the release rating
+    */
     private final String name;
 
     /**
-	 * Creates a {@code MovieRating} object with the given name
-	 * @param name
-	 */
+    * Creates a {@code MovieRating} object with the given name
+    * @param name
+    */
     private MovieRating(String name) {
         this.name = name;
     }
-    
-	/**
-	 * {@inheritDoc}
-	 */
+	
+    /**
+    * {@inheritDoc}
+    */
     @Override
     public String nameToString() {
         return name;
