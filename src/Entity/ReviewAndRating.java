5dafb6d43e859cbf5f0ae0b85fb2161aea9a2987
raydent30 <102127369+raydent30@users.noreply.github.com>
2022-10-30 11:19:46 +0800

    Update ReviewAndRating.java
    Added comments
    Some changes to the code
    Updated corresponding user --> customer
    Updated corresponding label --> name

--- src/Entity/ReviewAndRating.java
+++ src/Entity/ReviewAndRating.java
@@ -2,59 +2,94 @@ package Entity;
 
 import java.io.Serializable;
 
-// This class contains all information on reviews and ratings
+/**
+* This class contains all information on reviews and ratings
+*/
 public class ReviewAndRating implements Serializable, ItemName {
     /**
      * The serialisation version number <-- TO BE REVISITED
      */
     private static final long serialVersionUID = 1725162345547776214L;
 
-    // Minimum reviewer rating value
+    /**
+    * The minimum reviewer rating value
+    */
     public static final int MINIMUM_RATING = 1;
-
-    // Maximum reviewer rating value
+    
+    /**
+    * The maximum reviewer rating value
+    */
     public static final int MAXIMUM_RATING = 5;
-
-    // The user who made the review rating
-    private User user;
-    // The review
+    
+    /**
+    * The customer who made the review rating
+    */
+    private Customer customer;
+    
+    /**
+    * The review
+    */
     private String review;
-
-    // The rating
+    
+    /**
+    * The rating
+    */
     private Integer rating;
 
+    /**
+    * Creates a {@code ReviewAndRating} object
+    */
     private ReviewAndRating() {}
 
-    public static ReviewAndRating createReviewRating(User user, String review, int rating) {
+    /**
+    * Creates a {@code ReviewAndRating} object with the given customer, review and rating
+    * @param customer the customer who made the review rating
+    * @param review the review
+    * @param rating the rating
+    * @return the {@code ReviewAndRating} object if it's valid, null otherwise
+    */   
+    public static ReviewAndRating createReviewRating(Customer customer, String review, int rating) {
         ReviewAndRating reviewRating = new ReviewAndRating();
 
-        reviewRating.user = user;
+        reviewRating.customer = customer;
         reviewRating.review = review;
         reviewRating.rating = rating;
 
-        if (rating >= MINIMUM_RATING && rating <= MAXIMUM_RATING)
+        if (rating <= MAXIMUM_RATING && rating >= MINIMUM_RATING)
             return reviewRating;
         else
             return null;
     }
 
-    // Returns user that made the review and rating
-    public User getUser() {
-        return user;
+    /**
+    * Returns the customer who made the review rating
+    * @return the customer who made the review rating
+    */ 
+    public Customer getCustomer() {
+        return customer;
     }
 
-    // Returns review
+    /**
+    * Returns the review
+    * @return the review
+    */
     public String getReview() {
         return review;
     }
 
-    // Returns rating
+    /**
+    * Returns the rating
+    * @return the rating
+    */
     public int getRating() {
         return rating;
     }
 
+    /**
+    * {@inheritDoc}
+    */
     @Override
     public String nameToString() {
-        return review + " (" + rating + "/5) —— " + user.getName();
+        return review + " (" + rating + "/5) —— " + customer.getName();
     }
 }
