c6b9428cac92a83322746d000b933dc46aa507af
Lee Yen Foong Ernest <101156282+dihcuierc@users.noreply.github.com>
2022-10-30 16:20:48 +0800

    Add files via upload

--- src/Entity/AgeGroup.java
+++ src/Entity/AgeGroup.java
@@ -1,15 +1,25 @@
 package Entity;
 
+/**
+ * The different age groups available for ticket
+ */
 public enum AgeGroup implements ItemName{
 	SENIOR_CITIZEN("Senior Citizen"), ADULT("Adult"), CHILD("Child");
-	private String name;
+	private String ageGroup;
 	
-	private AgeGroup(String name) {
-		this.name = name;
+	/**
+	 * This creates a {@code AgeGroup} object with the input
+	 * @param name
+	 */
+	private AgeGroup(String ageGroup) {
+		this.ageGroup = ageGroup;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String nameToString() {
-		return name;
+		return ageGroup;
 	}
 }
