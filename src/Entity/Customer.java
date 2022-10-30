7ce17b859ff85f2ae5c7a17aeed31f1e82557df9
MangoTMH <80561272+MangoTMH@users.noreply.github.com>
2022-10-30 12:17:11 +0800

    Update Customer.java

--- src/Entity/Customer.java
+++ src/Entity/Customer.java
@@ -30,6 +30,11 @@ public class Customer implements Serializable{
     private String password;
     /**
      * Constructor for Customer's account
+     * @param username the username of the Customer's account
+     * @param name the name of the Customer
+     * @param mobileNumber the mobile/handphone number of the Customer
+     * @param emailAddress the email address of the Customer
+     * @param password the password of the Customer's account
      */
     public Customer(String username, String name, Integer mobileNumber, String emailAddress, String password) {
         this.username = username;
@@ -40,54 +45,64 @@ public class Customer implements Serializable{
     }
     /**
      * Function to check if correct password was entered
+     * @param password the password entered which will be checked against the Customer's account password
+     * @return true if password matches, false if passwords do not match
      */
     public boolean login(String password) {
         return password.equals(this.password);
     }
     /**
      * Getter for Customer's username
+     * @return username of the Customer
      */
     public String getUsername() {
         return this.username;
     }
     /**
      * Getter for Customer's name
+     * @return name of the Customer
      */
     public String getName() {
         return name;
     }
     /**
      * Setter for Customer's name
+     * @param name new name for the Customer
      */
     public void setName(String name) {
         this.name = name;
     }
     /**
      * Getter for Customer's mobile number
+     * @return mobile/handphone number of the Customer
      */
     public Integer getMobileNumber() {
         return mobileNumber;
     }
     /**
      * Setter for Customer's mobile number
+     * @param mobileNumber new mobile/handphone number of the Customer
      */
     public void setMobileNumber(Integer mobileNumber) {
         this.mobileNumber = mobileNumber;
     }
     /**
      * Getter for Customer's email address
+     * @return email address of the Customer
      */
     public String getEmailAddress() {
         return emailAddress;
     }
     /**
      * Setter for Customer's email address
+     * @param emailAddress new email address of the Customer
      */
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
     /**
      * Setter for Customer's password
+     * @param new password for the Customer's account
      */
     public void setPassword(String password) {
         this.password = password;
