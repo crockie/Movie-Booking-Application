81ac956f1992e6e871e7a7acd8eac823abd29e38
raydent30 <102127369+raydent30@users.noreply.github.com>
2022-10-30 09:51:01 +0800

    Update MovieRating.java
    Added meaning of the various movie ratings in comments
    Changed all related label --> name

--- src/Entity/MovieRating.java
+++ src/Entity/MovieRating.java
@@ -1,29 +1,57 @@
 package Entity;
 
-
-// Movie rating
-public enum MovieRating implements LabelledItem {
-
+/**
+ * The movie ratings 
+ */
+public enum MovieRating implements ItemName {
+   	/**
+	 * General
+	 */
     G("G"),
 
+    /**
+	 * Parental Guidance
+	 */
     PG("PG"),
 
+    /**
+	 * Parental Guidance 13
+	 */
     PG13("PG13"),
 
+    /**
+	 * No Children Under 16
+	 */
     NC16("NC16"),
 
+    /**
+	 * Mature 18
+	 */
     M18("M18"),
-
+	
+    /**
+	 * Restricted 21
+	 */
     R21("R21");
 
-    private final String label;
+    /**
+	 * The name of the release rating
+	 */
+    private final String name;
 
-    private MovieRating(String label) {
-        this.label = label;
+    /**
+	 * Creates a {@code MovieRating} object with the given name
+	 * @param name
+	 */
+    private MovieRating(String name) {
+        this.name = name;
     }
-
+    
+	/**
+	 * {@inheritDoc}
+	 */
     @Override
-    public String getLabel() {
-        return label;
+    public String nameToString() {
+        return name;
     }
 }
