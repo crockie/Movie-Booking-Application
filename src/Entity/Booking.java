05cca8e0f3da42c1240b3fb68fc2f9d48e75fefc
Lee Yen Foong Ernest <101156282+dihcuierc@users.noreply.github.com>
2022-10-30 16:48:36 +0800

    Add files via upload

--- src/Entity/Booking.java
+++ src/Entity/Booking.java
@@ -1,13 +1,39 @@
 package Entity;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Booking {
+/**
+ * This class contains all the information of a booking
+ */
+public class Booking implements Serializable {
+	/**
+	 * The serialisation version number
+	 */
+	private static final long serialVersionUID = 3967681838005218902L;
+	/**
+	 * The transaction id of the booking
+	 */
 	private String transactionId; 
+	/**
+	 * The customer who made the booking
+	 */
 	private Customer customer;
+	/**
+	 * An array of tickets selected in the booking
+	 */
 	private ArrayList<Ticket> tickets;
+	/**
+	 * The price of the booking
+	 */
 	private double price;
 	
-	// Creates a Booking object with transaction id, user, selected seats and price
+	/**
+	 * Creates a {@code Booking} object with a give transaction id, customer, selected seats and price
+	 * @param transactionId the transaction id of the booking
+	 * @param customer the customer who made the booking
+	 * @param selectedSeats the selected seats of the booking
+	 * @param price the price of the booking
+	 */
 	public Booking(String transactionId, Customer customer, boolean[][] selectedSeats, double price) {
 		this.transactionId = transactionId;
 		this.customer = customer;
@@ -23,18 +49,34 @@ public class Booking {
 		}
 	}
 	
+	/**
+	 * Get the transaction id of the booking
+	 * @return the transaction id of the booking
+	 */
 	public String getTransactionId() {
 		return transactionId;
 	}
 	
-	public Customer getUser() {
+	/**
+	 * Get the customer who made the booking
+	 * @return the customer who made the booking
+	 */
+	public Customer getCustomer() {
 		return customer;
 	}
 	
+	/**
+	 * Get the tickets booked in the booking
+	 * @return the tickets booked in the booking
+	 */
 	public ArrayList<Ticket> getTickets() {
 		return tickets;
 	}
 
+	/**
+	 * Get the price of the booking
+	 * @return the price of the booking
+	 */
 	public double getPrice() {
 		return price;
 	}	
