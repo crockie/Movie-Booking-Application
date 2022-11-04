aac1e046193e28c1625ee53a8b02089e05dbe913
AmosChong <amoschong0820@gmail.com>
2022-11-04 20:57:16 +0800

    Changes made according to ListView

--- src/control/TopMoviesControl.java
+++ src/control/TopMoviesControl.java
@@ -7,14 +7,15 @@ import java.util.Comparator;
 
 import entity.DatabaseManager;
 import entity.Movie;
-import view.ListView;
-import view.MenuView;
+import boundary.ListView;
+import boundary.MenuView;
 
 /**
- * This class controls the display of the top 5 movies by overall ticket sales or reviewer's rating
+ * This class controls the display of the top 5 movies by overall ticket sales
+ * or reviewer's rating
  */
-public class TopMoviesControl implements MainControl{
-	
+public class TopMoviesControl implements MainControl {
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -22,22 +23,21 @@ public class TopMoviesControl implements MainControl{
 	public void begin() {
 		while (true) {
 			int choice = MenuView.getMenuOption(
-				"Top 5 movies by: ",
-                "Total ticket sales",
-                "Overall reviewer's rating",
-				"Exit"
-			);
-			
+					"Top 5 movies by: ",
+					"Total ticket sales",
+					"Overall reviewer's rating",
+					"Exit");
+
 			List<String> movieStrings;
-			
+
 			switch (choice) {
-                case 1:
+				case 1:
 					movieStrings = getTopMoviesByTicketSales();
-					ListView.displayList("Top 5 Movies By Ticket Sales", movieStrings, "No available data");
+					ListView.showStringList("Top 5 Movies By Ticket Sales", movieStrings, "No available data");
 					break;
-                case 2:
+				case 2:
 					movieStrings = getTopMoviesByOverallRating();
-					ListView.displayList("Top 5 Movies By Overall Rating", movieStrings, "No available data");
+					ListView.showStringList("Top 5 Movies By Overall Rating", movieStrings, "No available data");
 					break;
 				case 3:
 					NavigateControl.popOne();
@@ -45,51 +45,59 @@ public class TopMoviesControl implements MainControl{
 			}
 		}
 	}
-	
+
 	/**
-	 * This method determines the top 5 movies by ticket sales and stores it as a list of strings.
-	 * @return a {@code List} of {@code String} objects containing the information about the top 5 movies by ticket sales
+	 * This method determines the top 5 movies by ticket sales and stores it as a
+	 * list of strings.
+	 * 
+	 * @return a {@code List} of {@code String} objects containing the information
+	 *         about the top 5 movies by ticket sales
 	 */
 	private List<String> getTopMoviesByTicketSales() {
 		List<Movie> movieList = DatabaseManager.getDataBase().getMovieList();
-		
+
 		List<Movie> movieList2 = new ArrayList<Movie>();
 		movieList2.addAll(movieList);
-		
+
 		Comparator<Movie> salesComparator = Collections.reverseOrder(Comparator.comparing(Movie::getTotalSales));
 		movieList2.sort(salesComparator);
-		
-		List<String> movieTitles = new ArrayList<String>(); 
-		
+
+		List<String> movieTitles = new ArrayList<String>();
+
 		for (int i = 1; i <= 5 && i <= movieList2.size(); i++) {
-			Movie movie = movieList2.get(i-1);
-			movieTitles.add(i + ". " + movie.getTitle() + " (Total Sales: $" + String.format("%.2f", movie.getTotalSales()) + ")");
+			Movie movie = movieList2.get(i - 1);
+			movieTitles.add(i + ". " + movie.getTitle() + " (Total Sales: $"
+					+ String.format("%.2f", movie.getTotalSales()) + ")");
 		}
 		return movieTitles;
 	}
-	
+
 	/**
-	 * This method determines the top 5 movies by overall reviewer's rating and stores it in the form of a list of strings.
-	 * @return a {@code List} of {@code String} objects containing the information about the top 5 movies by overall reviewer's rating
+	 * This method determines the top 5 movies by overall reviewer's rating and
+	 * stores it in the form of a list of strings.
+	 * 
+	 * @return a {@code List} of {@code String} objects containing the information
+	 *         about the top 5 movies by overall reviewer's rating
 	 */
 	private List<String> getTopMoviesByOverallRating() {
 		List<Movie> movieList = DatabaseManager.getDataBase().getMovieList();
 		List<Movie> moviesWithRatings = new ArrayList<Movie>();
-		
-		for (Movie movie: movieList) {
+
+		for (Movie movie : movieList) {
 			if (movie.getAverageRating() != null) {
-                moviesWithRatings.add(movie);
-            }
+				moviesWithRatings.add(movie);
+			}
 		}
-		
+
 		Comparator<Movie> ratingComparator = Collections.reverseOrder(Comparator.comparing(Movie::getAverageRating));
 		moviesWithRatings.sort(ratingComparator);
-		
+
 		List<String> movieTitles = new ArrayList<String>();
-		
+
 		for (int i = 1; i <= 5 && i < moviesWithRatings.size(); i++) {
-			Movie movie = moviesWithRatings.get(i-1);
-			movieTitles.add(i + ". " + movie.getTitle() + " (Average Ratings: " + String.format("%.2f", movie.getAverageRating()) + ")");
+			Movie movie = moviesWithRatings.get(i - 1);
+			movieTitles.add(i + ". " + movie.getTitle() + " (Average Ratings: "
+					+ String.format("%.2f", movie.getAverageRating()) + ")");
 		}
 		return movieTitles;
 	}
