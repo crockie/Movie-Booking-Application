7534c0906444efe52b2f91fd6ffe241b2ab413f4
AmosChong <amoschong0820@gmail.com>
2022-11-04 15:38:00 +0800

    Modifications made according to changes of DatabaseManager naming

--- src/control/BookingHistoryControl.java
+++ src/control/BookingHistoryControl.java
@@ -7,7 +7,7 @@ import entity.Booking;
 import entity.Cinema;
 import entity.Cineplex;
 import entity.Customer;
-import entity.DataController;
+import entity.DatabaseManager;
 import entity.MovieTime;
 import view.ListView;
 
@@ -16,18 +16,19 @@ import view.ListView;
  */
 public class BookingHistoryControl implements MainControl {
 	/**
-	 * The Customer that the booking history is being displayed for 
+	 * The Customer that the booking history is being displayed for
 	 */
 	private Customer customer;
-	
+
 	/**
 	 * Creates a new {@code BookingHistoryControl} object for the current Customer
+	 * 
 	 * @param customer the Customer that the booking history is being displayed for
 	 */
 	public BookingHistoryControl(Customer customer) {
 		this.customer = customer;
 	}
-	
+
 	/**
 	 * {@inheritDoc}
 	 * Displaying of Booking History for Customer
@@ -37,33 +38,35 @@ public class BookingHistoryControl implements MainControl {
 		ListView.displayList("Booking History", getBookingHistory(), "No bookings made by this customer");
 		NavigationController.goBack();
 	}
-	
+
 	/**
-	 * This function goes through all the cineplexes, cinemas, movie times and bookings to find the Customer's bookings
-	 * The information of the bookings is returned as a string 
-	 * @return a {@code List} of {@code String} objects containing the information about each of the Customer's bookings
+	 * This function goes through all the cineplexes, cinemas, movie times and
+	 * bookings to find the Customer's bookings
+	 * The information of the bookings is returned as a string
+	 * 
+	 * @return a {@code List} of {@code String} objects containing the information
+	 *         about each of the Customer's bookings
 	 */
 	public List<String> getBookingHistory() {
 		List<String> customerBookingHistory = new ArrayList<String>();
-		
-		for (Cineplex cineplex: DataController.getDataBase().getCineplexList()) {
-			for (Cinema cinema: cineplex.getCinemas()) {
-				for (MovieTime movieTime: cinema.getMovieTimes()) {
-					for (Booking booking: movieTime.getBookings()) {
+
+		for (Cineplex cineplex : DatabaseManager.getDataBase().getCineplexList()) {
+			for (Cinema cinema : cineplex.getCinemas()) {
+				for (MovieTime movieTime : cinema.getMovieTimes()) {
+					for (Booking booking : movieTime.getBookings()) {
 						if (booking.getCustomer() == customer) {
 							customerBookingHistory.add(
-								"Transaction ID: " + booking.getTransactionId() + "\n" +
-								"Price: $" + String.format("%.2f", booking.getPrice()) + "\n" +
-								"Movie: " + movieTime.getMovie().getTitle() + "\n" +
-								"Movie time: " + movieTime.nameToString() + "\n" +
-								"Cineplex: " + cineplex.getName() + "\n"
-							);
+									"Transaction ID: " + booking.getTransactionId() + "\n" +
+											"Price: $" + String.format("%.2f", booking.getPrice()) + "\n" +
+											"Movie: " + movieTime.getMovie().getTitle() + "\n" +
+											"Movie time: " + movieTime.nameToString() + "\n" +
+											"Cineplex: " + cineplex.getName() + "\n");
 						}
 					}
 				}
 			}
 		}
-		
+
 		return customerBookingHistory;
 	}
 }
