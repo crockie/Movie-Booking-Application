f38da413a21f552f3e394423ed72f2170c092ab5
raydent30 <102127369+raydent30@users.noreply.github.com>
2022-11-05 18:16:21 +0800

    Update CustomerLoginView.java

--- src/boundary/CustomerLoginView.java
+++ src/boundary/CustomerLoginView.java
@@ -7,38 +7,75 @@ import entity.*;
  */
 public class CustomerLoginView {
 	/**
+	 * This method reads in an entered password
+	 * @return the String password
+	 */
+	public static String inputPassword(String message) {
+		Console console = System.console();
+		if (console != null) {
+			char[] passwordArray = console.readPassword(message);
+		    return new String(passwordArray);
+
+		} 
+		else {
+			// Fallback
+			return readLine(message);
+		}
+	}
+	
+	/**
 	 * This method displays the signup form for the customer
 	 * @return the newly signed up customer
 	 */
 	public static Customer signupCustomer() {
-		String username = IOController.readLine("Username: ");
+		
+		System.out.print("Username: ");
+		Scanner sc = new Scanner(System.in);
+		String username = sc.nextLine();
 		
 		if (DatabaseManager.getDataBase().checkCustomerUsername(username)) {
-			IOController.displayMessage("Error: User with that username already exists");
+			System.out.println("Error: User with that username already exists");
 			return null;
 		}
 		
-		String name = IOController.readLine("Name: ");
+		System.out.print("Name: ");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
 		
-		int mobileNumber = IOController.readInt("Mobile Number: ");
-		String emailAddress = IOController.readLine("Email Address: ");
+		int mobileNumber=0;
+		while (true) {
+			try {
+				System.out.print("Mobile Number: ");
+				Scanner sc = new Scanner(System.in);
+				mobileNumber = sc.nextInt();
+				break;
+
+			} catch (InputMismatchException e) {
+				System.out.println("Please enter an integer");
+			}
+		}
+		
+		System.out.print("Email Address: ");
+		Scanner sc = new Scanner(System.in);	
+		String emailAddress = sc.nextLine();
 		
 		String password1 = "", password2;
 		
 		while (true){
-			password1 = IOController.readPassword("Password: ");
-			password2 = IOController.readPassword("Confirm Password: ");
+			password1 = inputPassword("Password: ");
+			password2 = inputPassword("Confirm Password: ");
 			
 			if (password1.equals(password2))
 				break;
 			else
-				IOController.displayMessage("Error: Password mismatch");
+				System.out.println("Error: Password mismatch");
 		}
+		
 		
 		Customer customer = new Customer(username, name, mobileNumber, emailAddress, password1);
 		
 		if (!DatabaseManager.getDataBase().addCustomer(customer))
-			IOController.displayMessage("Error: Unable to add customer");
+			System.out.println("Error: Unable to add customer");
 		
 		return customer;
 	}
@@ -48,19 +85,21 @@ public class CustomerLoginView {
 	 * @return the logged in customer
 	 */
 	public static Customer loginCustomer() {
-		String username = IOController.readLine("Username: ");
+		System.out.print("Username: ");
+		Scanner sc = new Scanner(System.in);
+		String username = sc.nextLine();
 		
 		if (!DatabaseManager.getDataBase().checkCustomerUsername(username)) {
-			IOController.displayMessage("Error: User with that that username doesn't exist");
+			System.out.println("Error: User with that that username doesn't exist");
 			return null;
 		}
 		
-		String password = IOController.readPassword("Password: ");
+		String password = inputPassword("Password: ");
 		
 		Customer customer = DatabaseManager.getDataBase().getCustomer(username, password);
 		
 		if (customer == null)
-			IOController.displayMessage("Error: Incorrect password");
+			System.out.println("Error: Incorrect password");
 		
 		return customer;
 	}
