7e315c98a14423cbd96ed98c622fccb61c7e8fad
MangoTMH <80561272+MangoTMH@users.noreply.github.com>
2022-11-11 21:00:46 +0800

    Update DisplayMovieControl.java

--- src/control/DisplayMovieControl.java
+++ src/control/DisplayMovieControl.java
@@ -6,6 +6,9 @@ import boundary.MovieView;
  * This class controls the display of the movies and their details and reviews.
  */
 public class DisplayMovieControl implements MainControl {
+    /**
+     * Shows if user is Customer or Staff
+     */
     private boolean isCustomer;
     /**
      * This constructor creates a new {@code MovieControl} object for the customer or staff
