87f90b83a9ac9527747aae98fa0f518f74b1c562
MangoTMH <80561272+MangoTMH@users.noreply.github.com>
2022-11-11 20:55:33 +0800

    Update TicketPriceControl.java

--- src/control/TicketPriceControl.java
+++ src/control/TicketPriceControl.java
@@ -5,6 +5,9 @@ import boundary.TicketPriceView;
  * This class controls the ticket price
  */
 public class TicketPriceControl implements MainControl {
+    /**
+	 * {@inheritDoc}
+	 */
     @Override
     public void begin() {
         TicketPriceView.displayAllTicketPrice();
