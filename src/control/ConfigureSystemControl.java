fa6fdb46bf869ec13a5794434583ea442c0a309a
MangoTMH <80561272+MangoTMH@users.noreply.github.com>
2022-11-11 21:03:10 +0800

    Update ConfigureSystemControl.java

--- src/control/ConfigureSystemControl.java
+++ src/control/ConfigureSystemControl.java
@@ -12,8 +12,8 @@ import entity.DatabaseManager;
 @SuppressWarnings("resource")
 public class ConfigureSystemControl implements MainControl {
     /**
-	 * {@inheritDoc}
-	 */
+     * {@inheritDoc}
+     */
     @Override
     public void begin() {
         System.out.println("Select the criteria you want users to list the movies by:");
